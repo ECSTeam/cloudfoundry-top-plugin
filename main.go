5b69f3623b572cd4ca184e604f5e73010a0dbf80
Kurt Kellner <kkellner@ecsteam.com>
2017-05-02 08:47:31 -0600

    Build version v0.8.5

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func (c *TopCmd) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 8,
-			Build: 4,
+			Build: 5,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
