cebbb2efd950c03b8e95a92801c6911167250d75
Kurt Kellner <kkellner@ecsteam.com>
2018-01-23 21:27:16 -0600

    Updated build / upload script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -43,37 +43,43 @@ WIN64_FILENAME="top-plugin64.exe"
 
 LINUX_ARM6_FILENAME="top-plugin-linux-arm6"
 
+echo "Compile ${LINUX32_FILENAME}"
 GOOS=linux GOARCH=386 go build -o "${LINUX32_FILENAME}"
 LINUX32_SHA1=`cat $LINUX32_FILENAME | openssl sha1`
 mkdir -p bin/linux32
 mv $LINUX32_FILENAME bin/linux32
 
+echo "Compile ${LINUX64_FILENAME}"
 GOOS=linux GOARCH=amd64 go build -o "${LINUX64_FILENAME}"
 LINUX64_SHA1=`cat $LINUX64_FILENAME | openssl sha1`
 mkdir -p bin/linux64
 mv $LINUX64_FILENAME bin/linux64
 
+echo "Compile ${MAC_FILENAME}"
 GOOS=darwin GOARCH=amd64 go build -o ${MAC_FILENAME}
 OSX_SHA1=`cat $MAC_FILENAME | openssl sha1`
 mkdir -p bin/osx
 mv $MAC_FILENAME bin/osx
 
+echo "Compile ${WIN32_FILENAME}"
 GOOS=windows GOARCH=386 go build -o ${WIN32_FILENAME}
 WIN32_SHA1=`cat $WIN32_FILENAME | openssl sha1`
 mkdir -p bin/win32
 mv $WIN32_FILENAME bin/win32
 
+echo "Compile ${WIN64_FILENAME}"
 GOOS=windows GOARCH=amd64 go build -o ${WIN64_FILENAME}
 WIN64_SHA1=`cat $WIN64_FILENAME | openssl sha1`
 mkdir -p bin/win64
 mv $WIN64_FILENAME bin/win64
 
+echo "Compile ${LINUX_ARM6_FILENAME}"
 GOOS=linux GOARCH=arm GOARM=6 go build -o $LINUX_ARM6_FILENAME
 LINUXARM6_SHA1=`cat $LINUX_ARM6_FILENAME | openssl sha1`
 mkdir -p bin/linux_arm6
 mv $LINUX_ARM6_FILENAME bin/linux_arm6
 
-
+echo "Compile complete"
 NOW=`TZ=UC date +'%Y-%m-%dT%TZ'`
 
 cat repo-index.yml |
@@ -90,39 +96,49 @@ cat
 
 if [[ "$1" = "release" ]] ; then
 
+	echo "Git commit and push"
+
 	git commit -am "Build version $TAG"
 	git push
+
+	echo "Create git release $TAG"
 
 	$GOPATH/bin/github-release release \
     --tag $TAG \
     --name "Cloud Foundry top plugin $TAG" \
     --description "$TAG release"
 
+	echo "Upload top-plugin-darwin"
 	$GOPATH/bin/github-release upload \
     --tag $TAG \
     --name "top-plugin-darwin" \
     --file bin/osx/top-plugin-darwin
 
+	echo "Upload top-plugin-linux32"
 	$GOPATH/bin/github-release upload \
     --tag $TAG \
     --name "top-plugin-linux32" \
     --file bin/linux64/top-plugin-linux32
 
+	echo "Upload top-plugin-linux64"
 	$GOPATH/bin/github-release upload \
     --tag $TAG \
     --name "top-plugin-linux64" \
     --file bin/linux64/top-plugin-linux64
 
+	echo "Upload top-plugin32.exe"
 	$GOPATH/bin/github-release upload \
     --tag $TAG \
     --name "top-plugin32.exe" \
     --file bin/win64/top-plugin32.exe
 
+	echo "Upload top-plugin64.exe"
 	$GOPATH/bin/github-release upload \
     --tag $TAG \
     --name "top-plugin64.exe" \
     --file bin/win64/top-plugin64.exe
 
+	echo "Upload top-plugin-linux-arm6"
 	$GOPATH/bin/github-release upload \
     --tag $TAG \
     --name "top-plugin-linux-arm6" \
